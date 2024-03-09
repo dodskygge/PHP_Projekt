b95cc7a4d0fa1841617498f227403442ae67ee05
dodskygge <makuch.oskar@gmail.com>
2024-03-09 01:29:51 +0100

    Shop start

--- components/header.php
+++ components/header.php
@@ -4,18 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ChipCom</title>
+    <title>ChipCom - sklep z elektroniką</title>
     <link rel="stylesheet" href="lib/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="css/global_style.css">
+    <link rel="stylesheet" href="css/header.css">
 </head>
 
 <body>
 
 <!-- HEADER MENU -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="header">
         <div class="container-fluid">
             <!-- Brand i przycisk "hamburger" dla urządzeń mobilnych -->
-            <a class="navbar-brand font-weight-bold" href="#">ChipCom</a>
+            <a class="navbar-brand font-weight-bold" href="#" id="logo">ChipCom</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -38,7 +40,19 @@
                     </li>
                 </ul>
 
+                <!--Searchbar-->
+                <div class="ml-2 mr-2" id="navbar-search">
+                    <input type="text" class="form-control" id="navbar-search-bar" placeholder="Szukaj...">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+
+                <!--Przyciski-->
                 <ul class="nav navbar-nav ml-auto">
+                    <li><button class="btn btn-outline-light mr-2" type="submit">
+                            <i class="fas fa-shopping-cart"></i>
+                            Cart
+                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                    </button></li>
                     <li><button class="btn btn-primary navbar-btn mr-2"><i class="fas fa-user"></i> Sign Up</button></li>
                     <li><button class="btn btn-success navbar-btn"><i class="fas fa-sign-in-alt"></i> Login</button></li>
                 </ul>
--- index.php
+++ index.php
@@ -1,3 +1,54 @@
 <?php include('components/header.php'); ?>
 
+        <!-- Shop container -->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    
+                <!-- Skrypt wyświetlający poszczególne itemy -->
+                <?php
+                    require('includes/db_conn.php');
+                   
+                    $sql = "SELECT product_name, product_price, product_price, product_quantity, product_category, image_url FROM products";
+                    $result = $conn->query($sql);
+
+                    // Sprawdzenie, czy są dane w bazie
+                    if ($result->num_rows > 0) {
+                        // Iteracja przez wyniki zapytania
+                        while($row = $result->fetch_assoc()) {
+                            ?>
+                            <div class="col mb-5">
+                                <div class="card h-100">
+                                    <!-- Product image-->
+                                    <img class="card-img-top" src="<?php echo $row['image_url']; ?>" alt="Product Image" />
+                                    <!-- Product details-->
+                                    <div class="card-body p-4">
+                                        <div class="text-center">
+                                            <!-- Product name-->
+                                            <h5 class="fw-bolder"><?php echo $row['product_name']; ?></h5>
+                                            <!-- Product price-->
+                                            $<?php echo $row['min_price']; ?> - $<?php echo $row['max_price']; ?>
+                                        </div>
+                                    </div>
+                                    <!-- Product actions-->
+                                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php
+                        }
+                    } else {
+                        echo "No products found in the database.";
+                    }
+
+                    // Zamknięcie połączenia z bazą danych
+                    $conn->close();
+                    ?>
+
+
+                </div>
+            </div>
+        </section>
+
 <?php include('components/footer.php'); ?>
